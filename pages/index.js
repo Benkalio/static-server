20f595cc706d2db5c6269d8347c4287ec7dfa1e7
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-03-24 18:27:50 +0100

    Pregeneration of static page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,15 @@
+import fs from 'fs/promises';
+import path from 'path'
+
 function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => <li key={product.id}>{product.title}</li>)}
+    </ul>
+  )
+
   return (
     <ul>
       <li>Product 1</li>
@@ -6,6 +17,19 @@ function HomePage(props) {
       <li>Product 3</li>
     </ul>
   );
+}
+
+//STATIC PAGE FOR PRE-GENERATION
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData)
+
+  return {
+    props: {
+      products: data.products
+    }
+  };
 }
 
 export default HomePage;
